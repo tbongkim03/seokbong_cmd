e26865a71f31db44738208be443cc3839c92b17b
tbongkim03 <tbongkim03@gmial.com>
2024-07-16 17:32:02 +0900

    seokbong_cmd.py

--- src/seokbong_cmd/seokbong_cmd.py
+++ src/seokbong_cmd/seokbong_cmd.py
@@ -1,14 +1,13 @@
-from seokbong_add.add import add
-from seokbong_division.divide import seokbong_division
+#from seokbong_add.add import add
+from seokbong_division.division import divide
 
 def call():
     choose = int(input("덧셈(1), 곱셈(2), 나눗셈(3) 입력 : "))
     if choose == 1:
-        add()
-    else if choose == 2:
-        mul()
-    else if choose == 3:
+        #add()
+        pass
+    elif choose == 2:
+        #mul()
+        pass
+    elif choose == 3:
         divide()
-    else:
-        print("num err")
-        break
