a1d0cf3901eb4c09f393e4f7e1dd0a7ac69ca122
tbongkim03 <tbongkim03@gmial.com>
2024-07-17 10:30:12 +0900

    change cmd.py

--- src/seokbong_cmd/seokbong_cmd.py
+++ src/seokbong_cmd/seokbong_cmd.py
@@ -1,9 +1,9 @@
-from seokbong_add.add import seokbong_add
+from seokbong_add.add import seokbong_plus
 from seokbong_division.division import seokbong_divide
 from seokbong_mul.mul import seokbong_mul
 
-def call_add():
-    seokbong_add()
+def call_plus():
+    seokbong_plus()
 
 def call_mul():
     seokbong_mul()
